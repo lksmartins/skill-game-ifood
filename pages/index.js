13f7b21ba5fcf7d0aaa11e2e07715f8aa8d00c97
lksmartins <lksmartins11@gmail.com>
2022-09-09 11:12:20 -0300

    index fit to mobile

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from './index.module.css'
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect'
 
 export default function Index() {
     return (
@@ -10,15 +11,23 @@ export default function Index() {
             <div className={styles.grid}>
 
                 <div className={styles.logo}>
-                    <Image src="/ifood-logo.svg" width="100" height="100" objectFit="contain"/>
+                    <Image src="/ifood-logo.svg" width="100" height="100" objectFit="contain" />
                 </div>
+                <MobileView>
+                    <div className={styles.image}>
+                        <Image src="/sacolinha_voando.svg" width="100" height="100" objectFit="contain" />
+                    </div>
+                </MobileView>
                 <div className={styles.text}>
                     <h1>Guia interativo de política de cancelamento da ifood</h1>
                     <Link href="/start"><a>Iniciar</a></Link>
                 </div>
-                <div className={styles.image}>
-                    <Image src="/ifood-logo.svg" width="100" height="100" objectFit="contain"/>
-                </div>
+                <BrowserView>
+                    <div className={styles.image}>
+                        <Image src="/ifood-logo.svg" width="100" height="100" objectFit="contain" />
+                    </div>
+                </BrowserView>
+
 
             </div>
 
