1ca0d95f564917115672884363967e77b5da1913
lksmartins <lksmartins11@gmail.com>
2022-09-09 11:20:28 -0300

    /start fit to mobile

--- pages/start.js
+++ pages/start.js
@@ -14,7 +14,7 @@ export default function start() {
                 </div>
                 <div className={styles.text}>
                     <p>Olá, bem-vindo ao guia interativo de política de cancelamento da iFood. Você irá interagir com uma apresentação gamificada, onde você irá escolher sua jornada de acordo com uma situação que ocorre ou que está ocorrendo em seu estabelecimento. Como este guia é interativo, a qualquer momento você poderá voltar e rever quantas situações desejar. Esperamos que este guia lhe ajude no seu processo de tomada de decisão e de melhor instruí-lo sobre o que fazer em situações de cancelamento.</p>
-                    <Link href="/jogo"><a>Iniciar minha jornada</a></Link>
+                    <Link href="/entrega"><a>Iniciar minha jornada</a></Link>
                 </div>
 
             </div>
