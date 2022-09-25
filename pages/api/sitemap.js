7d0e8cc64dd4536ffed607e0b45e44a04e7035bf
lksmartins <lksmartins11@gmail.com>
2022-09-25 11:08:43 -0300

    new sitemap

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,18 +1,46 @@
-const { SitemapStream, streamToPromise } = require('sitemap')
-const { Readable } = require('stream')
+import { SitemapStream, streamToPromise } from 'sitemap'
 
-// An array with your links
-const links = [
-    { url: '/', changefreq: 'monthly', priority: 0.5 },
-    { url: '/start', changefreq: 'monthly', priority: 0.5 },
-    { url: '/mapa/faco-minhas-entregas', changefreq: 'weekly', priority: 1 },
-    { url: '/mapa/uso-entregas-ifood', changefreq: 'weekly', priority: 1 },
-]
+export default async (req, res) => {
+    try {
+        const smStream = new SitemapStream({
+            hostname: `https://${req.headers.host}`,
+            cacheTime: 600000,
+        })
 
-// Create a stream to write to
-const stream = new SitemapStream({ hostname: 'https://skill-game-ifood.vercel.app/' })
+        const links = [
+            { url: '/', changefreq: 'monthly', priority: 0.5 },
+            { url: '/start', changefreq: 'monthly', priority: 0.5 },
+            { url: '/mapa/faco-minhas-entregas', changefreq: 'weekly', priority: 1 },
+            { url: '/mapa/uso-entregas-ifood', changefreq: 'weekly', priority: 1 },
+        ]
 
-// Return a promise that resolves with your XML string
-return streamToPromise(Readable.from(links).pipe(stream)).then((data) =>
-    data.toString()
-)
+        // Create each URL row
+        links.forEach(link => {
+            smStream.write({
+                url: link.url,
+                changefreq: link.changefreq,
+                priority: link.priority
+            })
+        })
+
+        // End sitemap stream
+        smStream.end()
+
+        // XML sitemap string
+        const sitemapOutput = (await streamToPromise(smStream)).toString()
+
+        // Change headers
+        res.writeHead(200, {
+            'Content-Type': 'application/xml'
+        })
+
+        // Display output to user
+        res.end(sitemapOutput)
+
+    }
+    catch (e) {
+        console.log(e)
+        res.send(JSON.stringify(e))
+    }
+
+}
