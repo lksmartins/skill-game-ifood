87ae8a2bcad98ba53a8f9181860d24a834afd3f5
lksmartins <lksmartins11@gmail.com>
2022-10-28 17:59:02 -0300

    layout update for mobile

--- pages/index.js
+++ pages/index.js
@@ -2,33 +2,38 @@ import React from 'react'
 import styles from './start.module.css'
 import Image from 'next/image'
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function start() {
 
-    return (
-        <main className={`${styles.start} container-fluid d-flex flex-column align-items-center justify-content-center p-5`}>
+    return (<>
 
-            <div className="container">
+        <Head>
+            <title>Escolha seu plano</title>
+        </Head>
+
+        <main className={`${styles.start} container-fluid d-flex flex-column align-items-center justify-content-center`}>
+
+            <div className="container m-0 p-2">
                 <div className="row">
                     <div className="col text-center">
                         <Link href="/">
-                            <a><Image src="/ifood-logo.svg" width="200" height="150" objectFit="contain" /></a>
+                            <a><Image src="/ifood-logo.svg" width="200" height="100" objectFit="contain" /></a>
                         </Link>
                     </div>
                 </div>
 
                 <div className="row">
-                    <div className="col fs-5 rounded p-5 mb-4" style={{backgroundColor:'#F9F2E8', color:'#5C5C5C'}}>
-                        Olá, loja parceira! Bem-vinda ao Guia Interativo - Política de Cancelamento para Restaurantes. Aqui, você vai simular situações reais envolvendo cancelamentos que ocorrem na jornada de pedidos na plataforma iFood.<br/>
-                        O nosso objetivo é que você entenda o processo de cancelamento e como ele impacta no seu negócio. Para iniciar, selecione que tipo de plano o do seu estabelecimento:
+                    <div className={`col fs-5 rounded p-4 mb-4 ${styles.mainText}`}>
+                        Olá parceiro! Escolha abaixo seu plano e veja situações que podem acontecer durante a jornada do pedido:
                     </div>
                 </div>
 
                 <div className="row">
                     <div className="col-lg-6 col-sm-12 p-0 pe-lg-2 mb-4">
                         <Link href="/mapa/faco-minhas-entregas">
-                            <a className={styles.startButton}>
-                                <div className={styles.image}>
+                            <a className={`${styles.startButton} btn-ifood-light`}>
+                                <div className={`${styles.image} d-none d-md-block`}>
                                     <img src="/start/basico.png" />
                                 </div>
                                 <div className={styles.innerText}>
@@ -41,8 +46,8 @@ export default function start() {
                     </div>
                     <div className="col-lg-6 col-sm-12 p-0 ps-lg-2">
                         <Link href="/mapa/uso-entregas-ifood">
-                            <a className={styles.startButton}>
-                                <div className={styles.image}>
+                            <a className={`${styles.startButton} btn-ifood-light`}>
+                                <div className={`${styles.image} d-none d-md-block`}>
                                     <img src="/start/entrega.svg" />
                                 </div>
                                 <div className={styles.innerText}>
@@ -57,5 +62,5 @@ export default function start() {
             </div>
 
         </main>
-    )
+    </>)
 }
