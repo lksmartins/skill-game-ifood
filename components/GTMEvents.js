c4a015d41a28ed2d7e862aff829209e86e472cd4
lksmartins <lksmartins11@gmail.com>
2022-12-08 18:02:18 -0300

    GA ID update fix

--- components/GTMEvents.js
+++ components/GTMEvents.js
@@ -2,7 +2,7 @@ import {useEffect} from 'react'
 import {useRouter} from 'next/router'
 
 //const trackerId = process.env.GOOGLE_GLOBAL_TAG
-const trackerId = 'G-Y95HE6QKFQ'
+const trackerId = 'G-HSK3TGXYZ6'
 
 export default function GTMEvents() {
 
